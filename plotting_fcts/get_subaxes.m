8345cca274d130ff413dee308a9319c4b9646e8d
MarcHafner <hafner.marc@gmail.com>
2014-03-20 11:05:04 -0400

    new function for subplot

---
+++ plotting_fcts/get_subaxes.m
@@ -0,0 +1,27 @@
+function h = get_subaxes(Nrows, Ncols, RowIdx, ColIdx, holded, offsets)
+% h = get_subaxes(Nrows, Ncols, RowIdx, ColIdx, holded, offsets)
+%   generate new axes such that it will fit (Nrows, Ncols) axes.
+%
+%   (Nrows, Ncols) are the number of rows and columns
+%   (RowIdx, ColIdx) are the indexes for row and column
+%
+%   holded is optional (default not hold)
+%   offsets is optional (default rather compact)
+%
+
+if ~exist('holded','var')
+    holded = false;
+end
+if ~exist('offsets','var')
+    offsets = [.08 .9 .06 .91];
+end
+
+
+temph = get_newaxes([offsets(1)+offsets(2)*(ColIdx-1)/Ncols ...
+    offsets(3)+offsets(4)*(RowIdx-1)/Nrows ...
+    -offsets(1)+(offsets(2)-.01)/Ncols ...
+    -offsets(3)+(offsets(4)-.01)/Nrows], holded);
+
+if nargout>0
+    h = temph;
+end
