76579571562e43a68c2763eb0f071dee7115fbe0
MarcHafner <hafner.marc@gmail.com>
2014-04-07 10:45:45 -0400

    new function to initialize

---
+++ small_helper/clearall.m
@@ -0,0 +1,4 @@
+clear all
+close all
+Generate_Plotting_parameters
+
--- small_helper/get_newfigure.m
+++ small_helper/get_newfigure.m
@@ -1,6 +1,9 @@
 function fhandle = get_newfigure(number, position, varargin)
 % fhandle = get_newfigure(number, position, varargin)
 %   get a clean new figure
+%   first argument of varargin can be the filename 
+%   further should be pairs of parameter/value as in set(gcf, ...)
+%
 
 if ~exist('position','var')
     position = [100 80 600 450];
@@ -12,6 +15,12 @@ else
     fhandle = figure(number);
 end
 clf
+
+if length(varargin)==1 && mod(length(varargin),2)==1 && varargin{1}(end-3)=='.'
+    varargin = ['filename' varargin];
+end
+
+
 set(gcf, 'color','w','paperunits','inches');
 set(gcf,'papersize', [position(3)/90 position(4)/90]);
 set(gcf, 'position', position, varargin{:});
