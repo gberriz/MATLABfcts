b255f36dc2d453e4b8ecef222de50872c9d331b1
MarcHafner <hafner.marc@gmail.com>
2014-04-11 17:14:36 -0400

    example script

---
+++ SingleCellFitting/test_subset_compiled_data.m
@@ -0,0 +1,12 @@
+% eample final processing
+
+final_data = filter_DeltaMOMP(compiled_data);    
+
+% example for choosing some conditions
+Idx = find(final_data.ExpKey.TRAIL==25 & final_data.ExpKey.ABT==0 );
+
+% extracting the specific conditions
+Exp_data = subset_compiled_data(final_data, Idx);
+
+% removing the trajectories after MOMP
+Exp_data = remove_postMOMP(Exp_data);
