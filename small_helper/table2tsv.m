a48d5bf51acb4db1ea1efdd036365f2309a4138a
MarcHafner <hafner.marc@gmail.com>
2014-04-24 21:14:41 -0400

    d300 combo functions

--- small_helper/table2tsv.m
+++ small_helper/table2tsv.m
@@ -1,5 +1,8 @@
 function table2tsv(a,filename)
 % table2tsv(a,filename)
 
+c = table2cell(a);
+c(cellfun(@iscategorical,c)) = cellfun2(@char,c(cellfun(@iscategorical,c)));
+
 cell2tsv(filename, [a.Properties.VariableNames;
-    table2cell(a)])
+    c])
