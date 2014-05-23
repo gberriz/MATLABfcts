cd35551bb1cc2bddf6334b38bcce899bf3c39bbd
MarcHafner <hafner.marc@gmail.com>
2014-05-22 20:14:36 -0400

    SEM for NaN and matrices

--- calculations/SEM.m
+++ calculations/SEM.m
@@ -1,7 +1,28 @@
-function SEM_value = SEM(values)
+function SEM_value = SEM(values,dim)
+
+if exist('dim','var') && dim==2
+    values = values';
+    SEM_value = SEM(values);
+    SEM_value = SEM_value';
+    return
+end
+
+if all(size(values)>1)
+    SEM_value = NaN(1,size(values,2));
+    for i=1:size(values,2) 
+        idx = ~isnan(values(:,i));
+        if sum(idx)<2, continue, end
+        SEM_value(i) = SEM(values(idx,i));
+    
+    end
+    return
+end
+
+values = ToColumn(values);
 
 if length(values)>2
     SEM_value = std(values)/sqrt(length(values));
 else
     SEM_value = abs(diff(values)/2);
 end
+
