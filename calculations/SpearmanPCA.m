d23712d7a367c5c8c1b7d8de5ddef749ad560851
MarcHafner <hafner.marc@gmail.com>
2014-07-28 17:39:00 -0400

    improved efficiency

--- calculations/SpearmanPCA.m
+++ calculations/SpearmanPCA.m
@@ -1,14 +1,36 @@
-function [coeff, scores, explained, latent] = SpearmanPCA(data)
+function [coeff, scores, explained, latent] = SpearmanPCA(data, k)
 
 r = corr(data,'type','spearman');
 
-coeff = zeros(size(r));
-latent = zeros(size(r,1),1);
-explained = zeros(size(r,1),1);
+coeff = zeros(size(data,2));
+latent = zeros(size(data,2),1);
+explained = zeros(size(data,2),1);
 
 idx = ~all(isnan(r));
 
-[coeff(idx,1:length(idx)), latent(1:length(idx)), explained(1:length(idx))] = pcacov(r(idx,idx));
+if exist('k','var')
+    assert(k<sum(idx))
+    [~,l,c] = svds(r(idx,idx), k);
+    latent(1:k) = diag(l);
+    
+    explained = 100*latent/sum(latent);
+    
+    % Enforce a sign convention on the coefficients -- the largest element in each
+    % column will have a positive sign.
+    [p,d] = size(c);
+    [~,maxind] = max(abs(c),[],1);
+    colsign = sign(c(maxind + (0:p:(d-1)*p)));
+    coeff(idx,1:k) = bsxfun(@times,c,colsign);
+    
+else
+    if sum(idx)>5e3
+        warning('Option iwth limited number of components will be faster')
+    end
+    [coeff(idx,1:sum(idx)), latent(1:sum(idx)), explained(1:sum(idx))] = pcacov(single(r(idx,idx)));
+end
 
-scores = zeros(size(data));
-scores(:,idx) = data(:,idx)*coeff(:,idx);
+scores = (data-repmat(mean(data),size(data,1),1))*coeff;
+
+
+
+
