17559ed2cf67b1251bb8ad491f48f4ead2c00379
MarcHafner <hafner.marc@gmail.com>
2014-08-18 16:10:09 -0400

    can handle arrays with multiple rows

--- small_helper/cellstr2str.m
+++ small_helper/cellstr2str.m
@@ -13,6 +13,14 @@ if isempty(cell_in)
     return
 end
 
+if size(cell_in,1)>1
+    str_out = cell(size(cell_in,1),1);
+    for i=1:size(cell_in,1)
+        str_out{i} = cellstr2str(cell_in(i,:), spacer);
+    end
+    return
+end
+
 str_out = cell_in{1};
 for i=2:length(cell_in)
     str_out = [str_out spacer cell_in{i}];
