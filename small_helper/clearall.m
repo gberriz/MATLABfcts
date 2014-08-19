424a33e0c3a6fd702cfd3da65381cd2e86a057d4
MarcHafner <hafner.marc@gmail.com>
2014-08-19 12:23:02 -0400

    adding path to Git folder

--- small_helper/clearall.m
+++ small_helper/clearall.m
@@ -3,3 +3,11 @@ close all
 Generate_Plotting_parameters
 clc
 
+global GitFolder
+if exist('d:\GitHub\','dir')
+    GitFolder = 'd:\GitHub\';
+elseif exist('m:\GitHub\','dir')
+    GitFolder = 'm:\GitHub\';
+end
+
+
