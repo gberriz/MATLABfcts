8ee426f5fc2ad131f0be540995665324e50e3396
MarcHafner <hafner.marc@gmail.com>
2014-08-21 14:39:48 -0400

    cleaning up

--- small_helper/table2cellstr.m
+++ small_helper/table2cellstr.m
@@ -1,13 +1,6 @@
 function cstr = table2cellstr(t_in, headers)
+% cstr = table2cellstr(t_in, headers)
 
-% for i=1:size(a,2)
-%     if iscategorical(a.(a.Properties.VariableNames{i})(1))
-%         a.(a.Properties.VariableNames{i}) = cellstr(a.(a.Properties.VariableNames{i}));
-%     elseif isnumeric(a.(a.Properties.VariableNames{i})(1))
-%         a.(a.Properties.VariableNames{i}) = cellfun_brdcast(@num2str,...
-%             num2cell(a.(a.Properties.VariableNames{i})));
-%     end
-% end
 t_in = TableToString(t_in, 0);
 
 if ~exist('headers','var') || headers
