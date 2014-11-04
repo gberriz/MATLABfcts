50956919745f75660ffb7ac4ea64ea6c11c464ee
MarcHafner <hafner.marc@gmail.com>
2014-11-04 17:18:37 -0500

    default -> all cols

--- src/small_helper/TableToCategorical.m
+++ src/small_helper/TableToCategorical.m
@@ -2,9 +2,12 @@ function t_out = TableToCategorical(t_in, varidx)
 % TableToCategorical(t_in, varidx)
 %   change the columns in category arrays.
 %       varidx is the columns to change (either Variablenames or indices)
-%       set varidx=0 for all string columns
+%       set varidx=0 for all string columns (default)
 %
 
+if ~exist('varidx')
+    varidx=0;
+end
 if isnumeric(varidx)
     if varidx==0
         varidx = t_in.Properties.VariableNames;
