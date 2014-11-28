27130509fdf9abfb7b7e3ae30c0c833b6def2fb9
MarcHafner <hafner.marc@gmail.com>
2014-11-28 18:00:05 -0500

    normalization functions

---
+++ src/small_helper/NormRangeUnit.m
@@ -0,0 +1,13 @@
+function mat_out = NormRangeUnit(mat_in, dim)
+% mat_out = NormRangeUnit(mat_in, dim)
+%   normalized each column (or along dimension  dim  ) to range from 0 to 1
+%
+
+if ~exist('dim','var')
+    dim = 1;
+end
+dims = ones(1,length(size(mat_in)));
+dims(dim) = size(mat_in,dim);
+ 
+mat_in = mat_in - repmat(min(mat_in,[],dim), dims);
+mat_out = mat_in ./ repmat(max(mat_in,[],dim), dims);
--- src/small_helper/NormSumUnit.m
+++ src/small_helper/NormSumUnit.m
@@ -1,9 +1,12 @@
-function mat_out = NormUnit(mat_in, dim)
+function mat_out = NormSumUnit(mat_in, dim)
+% mat_out = NormSumUnit(mat_in, dim)
+%   normalized each column (or along dimension  dim  ) to have a sum of 1
+%
 
 if ~exist('dim','var')
     dim = 1;
 end
 dims = ones(1,length(size(mat_in)));
 dims(dim) = size(mat_in,dim);
- 
+
 mat_out = mat_in ./ repmat(sum(mat_in,dim), dims);
