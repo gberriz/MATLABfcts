acd3e878be538e3ea199cec09bef142c11e7dc35
Marc Hafner <hafner.marc@gmail.com>
2014-12-20 21:20:35 -0500

    report permuts

--- src/plotting_fcts/DendrogramWithBars.m
+++ src/plotting_fcts/DendrogramWithBars.m
@@ -1,7 +1,9 @@
-function [axis_handles, bar_handles] = DendrogramWithBars(SampleDist, t_SampleLabels, AxisPos, xwidth, ...
+function [axis_handles, bar_handles, outperm, groups] = DendrogramWithBars(SampleDist, t_SampleLabels, AxisPos, xwidth, ...
   PlotOptions, colorthreshold)
-% function [axis_handles, bar_handles] = DendrogramWithBars(SampleDist, t_SampleLabels, AxisPos, xwidth, ...
+% function [axis_handles, bar_handles, outperm, groups] = DendrogramWithBars(SampleDist, t_SampleLabels, AxisPos, xwidth, ...
 %   PlotOptions, colorthreshold)
+%
+%   if colorthreshold is negative, set as quantile of the distances.
 
 labels = cellstr2str( table2cellstr(t_SampleLabels,0));
 
@@ -15,6 +17,9 @@ if ~exist('colorthreshold','var') || isempty(colorthreshold)
     [h,~,outperm] = dendrogram(tree,0,'labels',labels,'orientation','left');
     for i=1:length(h), set(h(i),'color','k'),end
 else
+    if colorthreshold<0
+        colorthreshold = quantile(tree(:,3), -colorthreshold);
+    end
     [h,~,outperm] = dendrogram(tree,0,'labels',labels,'orientation','left',...
         'colorthreshold',colorthreshold);
     SampleColor = NaN(length(labels),3);
@@ -34,11 +39,13 @@ end
 ylabs = get(gca,'yticklabel');
 set(gca,'fontsize',8, 'ytick',[])
 ylim([.5 length(labels)+.5])
-xlim([0 1.1])
+xlim([-.2 1.1])
 
 %%
 fields = varnames(t_SampleLabels);
 bar_handles = cell(length(fields),1);
+groups = ones(height(t_SampleLabels),1);
+cnt = 1;
 for iF = 1:length(fields)
     axis_handles(iF+1) = get_newaxes([AxisPos(1)+AxisPos(3)+(iF-1)*xwidth AxisPos(2) xwidth AxisPos(4)],1);
     
@@ -53,6 +60,8 @@ for iF = 1:length(fields)
     if exist('SampleColor','var')
         for i = find(any(diff(SampleColor,1)~=0,2))'
             plot([0 1], [i i]+.5, '-k')
+            cnt = cnt+1;
+            groups((i+1):end) = cnt;
         end
     end
     
