8941f12d73269547c2120dd62135b5d14c9c08c4
MarcHafner <hafner.marc@gmail.com>
2015-02-19 09:13:42 -0500

    more robust: handle \ in string

--- src/small_helper/cell2csv.m
+++ src/small_helper/cell2csv.m
@@ -17,10 +17,17 @@ logicidx = cellfun(@islogical,data(:));
 data(logicidx) = cellfun(@num2str,data(logicidx),'uniformoutput',0);
 
 
-data(:,1:end-1) = strcat(data(:,1:end-1),delimiter);
 
 file = fopen(filename,'w');
 for i=1:size(data,1)
+    idx = strfindcell(data(i,:),'\')>0;
+    if any(idx)
+        for j = find(idx)
+            data{i,j} = strjoin(strcat(regexp(data{i,j},'\','split'),'\\'),'');
+        end
+    end
+    
+    data(i,1:end-1) = strcat(data(i,1:end-1),delimiter);
     fprintf(file,[data{i,:}]);
     if i<size(data,1)
         fprintf(file,'\n');
