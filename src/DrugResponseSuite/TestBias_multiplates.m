60d21b2b37ae6a3b95905a840414ba1360c19ffb
MarcHafner <hafner.marc@gmail.com>
2015-04-23 15:07:11 -0400

    New functions for testing bias

---
+++ src/DrugResponseSuite/TestBias_multiplates.m
@@ -0,0 +1,36 @@
+function BiasValue = TestBias_multiplates(t_data, BiasCutoff, plotting, valvars)
+
+if ~exist('BiasCutoff','var') || isempty(BiasCutoff)
+    BiasCutoff = [1;1;1]*[.1 .01];
+elseif all(size(BiasCutoff)==[1 2])
+    BiasCutoff = [1;1;1]*BiasCutoff;
+elseif ~all(size(BiasCutoff)==[3 2])
+    error('Wrong size for BiasCutoff input, needs to be 1x2 or 3x2')
+end
+
+
+if ~exist('plotting','var') || isempty(plotting)
+    plotting = .5;
+end
+
+if ~exist('valvars','var') || isempty(valvars)
+    valvars = 'Cellcount';
+end
+
+plates = unique(t_data.Barcode);
+
+BiasValue = zeros(length(plates), 3);
+
+for ip = 1:length(plates)
+    t_plate = t_data(t_data.Barcode==plates(ip),intersect(varnames(t_data), ...
+        [{'Barcode' 'Column' 'Row' 'Well'} valvars]));
+    
+    bias_res = cell(1,3);
+    [biased, bias_res{:}] = TestPlateBias(t_plate, BiasCutoff, plotting);
+    
+    for i=find(biased)
+        BiasValue(ip,i) = min(BiasValue(ip,i), min(bias_res{i}(...
+            bias_res{i}(:,1)<-BiasCutoff(i,1) & bias_res{i}(:,3)<BiasCutoff(i,2),1)));
+    end
+end
+
