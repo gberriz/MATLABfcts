f1aa599ebf188e79cb50ae9432a5bb4598db0c0d
Marc Hafner <hafner.marc@gmail.com>
2015-06-25 20:59:20 -0400

    some rewriting

--- src/DrugResponseSuite/TreatmentDesign.m
+++ src/DrugResponseSuite/TreatmentDesign.m
@@ -51,24 +51,22 @@ addParameter(p,'max_DMSOpct', .2, @(x) isscalar(x) && isnumeric(x)); % maximum 0
 
 parse(p, varargin{:})
 p = p.Results;
-for i = {'ComboDoses' 'ComboLists' 'DrugPairs' 'Seed' 'edge_ctrl' 'stock_conc' ...
-        'well_volume' 'plate_dims' 'min_volume' 'step_volume' 'max_DMSOpct'}
-    eval([i{:} ' = p.' i{:} ';'])
-end
+
 % avoid too high level of DMSO (max is 0.2%)
-max_volume = well_volume*max_DMSOpct/100;
+max_volume = p.well_volume*p.max_DMSOpct/100;
 
 %% checks
 DrugNames = ToColumn(DrugNames);
 assert((length(DrugNames)==length(HMSLids)) || isempty(HMSLids))
 assert(length(DrugNames)==length(SingleDoses))
 
-assert(isempty(DrugPairs) || (~isempty(ComboDoses) || ~isempty(ComboLists)))
-assert(all(DrugPairs(:)<=length(DrugNames)) || isempty(DrugPairs))
-assert((length(DrugNames)==length(ComboDoses)) || isempty(ComboDoses))
-assert(size(DrugPairs,2)==2)
-assert(all(size(DrugPairs)==size(ComboLists)) || isempty(ComboLists))
+assert(isempty(p.DrugPairs) || (~isempty(p.ComboDoses) || ~isempty(p.ComboLists)))
+assert(all(p.DrugPairs(:)<=length(DrugNames)) || isempty(p.DrugPairs))
+assert((length(DrugNames)==length(p.ComboDoses)) || isempty(p.ComboDoses))
+assert(size(p.DrugPairs,2)==2)
+assert(all(size(p.DrugPairs)==size(p.ComboLists)) || isempty(p.ComboLists))
 
+stock_conc = p.stock_conc;
 assert((length(DrugNames)==length(stock_conc)) || (length(stock_conc)==1))
 assert(all(stock_conc>20 & stock_conc<5e4), 'Stock concentration should be in uM')
 if length(stock_conc)==1
@@ -76,19 +74,20 @@ if length(stock_conc)==1
 end
 stock_conc = num2cell(ToColumn(stock_conc));
 
-if length(plate_dims)==1
-    plate_dims = sqrt(plate_dims*[1/1.5 1.5]);
+if length(p.plate_dims)==1
+    p.plate_dims = sqrt(p.plate_dims*[1/1.5 1.5]);
 end
-assert(round(log2(plate_dims(1)))==log2(plate_dims(1)))
+warnassert(round(log2(p.plate_dims(1)))==log2(p.plate_dims(1)), ...
+    'Non-standard plate size; edge_ctrl may be not working properly')
 
-nWells = prod(plate_dims);
-if well_volume<1e2 && nWells<100
+nWells = prod(p.plate_dims);
+if p.well_volume<1e2 && nWells<100
     warnprintf('Default volume is %.0f set for 384-well plate; check if correct', ...
-        well_volume)
+        p.well_volume)
 end
-assert(well_volume>1 && well_volume<1e4, 'Well volume should be given in uL')
+assert(p.well_volume>1 && p.well_volume<1e4, 'Well volume should be given in uL')
 
-if Seed==0 && nReps>1
+if p.Seed==0 && nReps>1
     warnprintf('Seed is set to 0 (i.e. no randomization) and Nreps>1')
     warnprintf('This means all replicates will have the same layout!')
     warnprintf('Confirm to proceed')
@@ -100,33 +99,33 @@ end
 
 
 Drugs = struct('DrugName', DrugNames, 'HMSLid', ToColumn(HMSLids),...
-    'stock_conc', stock_conc, 'layout', zeros(plate_dims));
-Designs = struct('plate_dims', repmat({plate_dims}, nReps, 1), ...
-    'treated_wells', repmat({true(plate_dims)}, nReps, 1), ...
-    'well_volume', repmat({well_volume}, nReps, 1), ...
-    'Drugs', repmat({Drugs}, nReps, 1), 'Seed', num2cell(Seed-1+(1:nReps)'));
+    'stock_conc', stock_conc, 'layout', zeros(p.plate_dims));
+Designs = struct('plate_dims', repmat({p.plate_dims}, nReps, 1), ...
+    'treated_wells', repmat({true(p.plate_dims)}, nReps, 1), ...
+    'well_volume', repmat({p.well_volume}, nReps, 1), ...
+    'Drugs', repmat({Drugs}, nReps, 1), 'Seed', num2cell(p.Seed-1+(1:nReps)'));
 
-SingleDoses = cellfun2(@(x,y,z) round_Doses(x,y,z,'Single',min_volume, ...
-    step_volume, max_volume, well_volume), SingleDoses, stock_conc, DrugNames);
+SingleDoses = cellfun2(@(x,y,z) round_Doses(x,y,z,'Single',p.min_volume, ...
+    p.step_volume, max_volume, p.well_volume), SingleDoses, stock_conc, DrugNames);
 
-if isempty(ComboLists) && ~isempty(ComboDoses)
-    ComboLists = [ComboDoses(DrugPairs(:,1)) ComboDoses(DrugPairs(:,2))];
+if isempty(p.ComboLists) && ~isempty(p.ComboDoses)
+    p.ComboLists = [p.ComboDoses(p.DrugPairs(:,1)) p.ComboDoses(p.DrugPairs(:,2))];
 end
 
-if ~isempty(ComboLists)
-    ComboLists(:) = cellfun2(@(x,y,z) round_Doses(x,y,z,'Combo',min_volume, ...
-        step_volume, max_volume, well_volume), ComboLists(:), ...
-        stock_conc(DrugPairs(:)), DrugNames(DrugPairs(:)));
+if ~isempty(p.ComboLists)
+    p.ComboLists(:) = cellfun2(@(x,y,z) round_Doses(x,y,z,'Combo',p.min_volume, ...
+        p.step_volume, max_volume, p.well_volume), p.ComboLists(:), ...
+        stock_conc(p.DrugPairs(:)), DrugNames(p.DrugPairs(:)));
     fprintf('\n')
     for iD = 1:length(SingleDoses)
-        if any(~ismember([ComboLists{DrugPairs==iD}], SingleDoses{iD}))
+        if any(~ismember([p.ComboLists{p.DrugPairs==iD}], SingleDoses{iD}))
             warnprintf('Some doses for the combo are not part of the single doses for %s', ...
                 DrugNames{iD})
         end
     end
     nTreatments = sum(cellfun(@length,SingleDoses)) + ...
-        sum(cellfun(@(x,y) length(x)*length(y), ComboLists(:,1), ...
-        ComboLists(:,2)));
+        sum(cellfun(@(x,y) length(x)*length(y), p.ComboLists(:,1), ...
+        p.ComboLists(:,2)));
 else
     nTreatments = sum(cellfun(@length,SingleDoses));
 end
@@ -140,15 +139,15 @@ assert(ctrl_cnt>5, 'Too many well used (%i out of %i), need at least 6 control w
     nTreatments, nWells)
 
 
-if edge_ctrl
-    [ctrlidx, treated_wells] = DefineFixedControlPositions(plate_dims, ctrl_cnt);
-    for iR=1:nReps
-        Designs(iR).treated_wells = treated_wells;
-    end
+if p.edge_ctrl
+    [ctrlidx, treated_wells] = DefineFixedControlPositions(p.plate_dims, ctrl_cnt);
 else
     ctrlidx = [];
+    treated_wells = true(p.plate_dims);
 end
-
+for iR=1:nReps
+    Designs(iR).treated_wells = treated_wells;
+end
 %% define all possible treatments
 allTreatments = zeros(length(DrugNames), nTreatments);
 cnt = 0;
@@ -156,13 +155,13 @@ for iD = 1:length(DrugNames)
     allTreatments(iD, cnt+(1:length(SingleDoses{iD}))) = SingleDoses{iD};
     cnt = cnt + length(SingleDoses{iD});
 end
-for iCo = 1:size(DrugPairs,1)
-    for iD1 = 1:length(ComboLists{iCo,1})
-        allTreatments(DrugPairs(iCo,1), cnt+(1:length(ComboLists{iCo,2}))) = ...
-            ComboLists{iCo,1}(iD1);
-        allTreatments(DrugPairs(iCo,2), cnt+(1:length(ComboLists{iCo,2}))) = ...
-            ComboLists{iCo,2};
-        cnt = cnt + length(ComboLists{iCo,2});
+for iCo = 1:size(p.DrugPairs,1)
+    for iD1 = 1:length(p.ComboLists{iCo,1})
+        allTreatments(p.DrugPairs(iCo,1), cnt+(1:length(p.ComboLists{iCo,2}))) = ...
+            p.ComboLists{iCo,1}(iD1);
+        allTreatments(p.DrugPairs(iCo,2), cnt+(1:length(p.ComboLists{iCo,2}))) = ...
+            p.ComboLists{iCo,2};
+        cnt = cnt + length(p.ComboLists{iCo,2});
     end
 end
 
@@ -174,15 +173,15 @@ assert(all(any(allTreatments>0)))
 for iR = 1:nReps
     
     Designs(iR).Drugs = RandomizePlatePositions(Designs(iR).Drugs, ...
-        allTreatments, nWells, plate_dims, ctrlidx, ctrl_cnt, Seed+iR-1);
+        allTreatments, nWells, p.plate_dims, ctrlidx, ctrl_cnt, p.Seed+iR-1);
     
-    allDrugs = reshape([Designs(iR).Drugs.layout], [plate_dims length(DrugNames)]);
+    allDrugs = reshape([Designs(iR).Drugs.layout], [p.plate_dims length(DrugNames)]);
     nDrugs = sum(allDrugs>0,3);
     assert(all(squeeze(sum(sum((allDrugs>0).*repmat(nDrugs==1,1,1,length(DrugNames)),2),1))==...
         cellfun(@length,SingleDoses)))
-    for iCo = 1:size(DrugPairs,1)
-        assert(sum(sum( all(allDrugs(:,:,DrugPairs(iCo,:))>0,3)))== ...
-            length(ComboLists{iCo,1})*length(ComboLists{iCo,2}))
+    for iCo = 1:size(p.DrugPairs,1)
+        assert(sum(sum( all(allDrugs(:,:,p.DrugPairs(iCo,:))>0,3)))== ...
+            length(p.ComboLists{iCo,1})*length(p.ComboLists{iCo,2}))
     end
     
 end
